49d0398bb9c9f46c7239abc1ff4d55c14e5703fe
vicky <jiazixia@bigai.ai>
2023-10-26 17:22:05 +0800

    new

--- Automatical_Relation_Generation/run.sh
+++ Automatical_Relation_Generation/run.sh
@@ -1,4 +1,4 @@
-DATA_FILE=./resource/DocRED/train_annotated
+DATA_FILE = ./resource/DocRED/train_annotated
 
 python I_gpt_proposal.py -i ${DATA_FILE}.json -o ${DATA_FILE}_gpt
 
@@ -7,5 +7,6 @@ python II_gpt_triples_postprocess.py -i ${DATA_FILE}_gpt -o ${DATA_FILE}_gpt_pos
 
 
 python III_nli_annotator.py -i ${DATA_FILE}_gpt_postprocess -o ${DATA_FILE}_nli
+
 
 python IV_nli_score_postprocess.py -origin ${DATA_FILE}.json -i ${DATA_FILE}_nli -o ./resource/enhancement_data/docred_train_data_enhancement.json
